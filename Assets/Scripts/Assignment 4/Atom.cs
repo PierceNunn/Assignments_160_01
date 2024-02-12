3b11efa7e4131c9f0090f861dfe43cbe9104da29
PierceNunn <agentp5151@gmail.com>
2024-02-11 23:18:06 -0600

    finished assignment 4

--- Assets/Scripts/Assignment 4/Atom.cs
+++ Assets/Scripts/Assignment 4/Atom.cs
@@ -1,3 +1,11 @@
+/*****************************************************************************
+// File Name : Atom.cs
+// Author : Pierce Nunnelley
+// Creation Date : February 11, 2024
+//
+// Brief Description : This script established the Atom class, which has 
+values protons and electrons, as well as a print.
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,29 +15,43 @@ public class Atom : MonoBehaviour
     [SerializeField] private int protons;
     [SerializeField] private int electrons;
 
-    public int Protons { get => protons; set => protons = value; }
-    public int Electrons { get => electrons; set => electrons = value; }
+    protected int Protons { get => protons; set => protons = value; }
+    protected int Electrons { get => electrons; set => electrons = value; }
 
-
+    /// <summary>
+    /// Constructor for the Atom class.
+    /// </summary>
+    /// <param name="p">value to set protons.</param>
+    /// <param name="e">value to set electrons.</param>
     public Atom(int p, int e)
     {
         protons = p;
         electrons = e;
     }
 
+    /// <summary>
+    /// prints the values for Protons and Electrons of the atom.
+    /// </summary>
     public virtual void PrintAtom()
     {
         print("Number of Protons: " + protons + "\n Number of Electrons: "
             + electrons);
     }
+
+    /// <summary>
+    /// runs PrintAtom on start.
+    /// </summary>
     void Start()
     {
-        
+        PrintAtom();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Sets protons and electrons to 0.
+    /// </summary>
+    private void OnDestroy()
     {
-        
+        protons = 0;
+        electrons = 0;
     }
 }
--- Assets/Scripts/Assignment 4/Hydrogen.cs
+++ Assets/Scripts/Assignment 4/Hydrogen.cs
@@ -1,18 +1,33 @@
+/*****************************************************************************
+// File Name : Hydrogen.cs
+// Author : Pierce Nunnelley
+// Creation Date : February 11, 2024
+//
+// Brief Description : This script establishes the Hydrogen class, deriving
+from the Atom class. it also overrides the PrintAtom function.
+*****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hydrogen : Atom
 {
-
-    void Start()
+    /// <summary>
+    /// Constructor for the Hydrogen class.
+    /// </summary>
+    /// <param name="p">value to set protons.</param>
+    /// <param name="e">value to set electrons.</param>
+    public Hydrogen(int p, int e) : base(p, e)
     {
-        
+        Protons = p;
+        Electrons = e;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Prints the number of protons in the Hydrogen.
+    /// </summary>
+    public override void PrintAtom()
     {
-        
+        print("Number of Protons: " + Protons);
     }
 }
